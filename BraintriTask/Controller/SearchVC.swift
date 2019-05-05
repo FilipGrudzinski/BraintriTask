ca182362a7f814601cd806bba4a3e76e1b68253b
FilipGrudzinski <grudzinski.filip@gmail.com>
2019-05-05 18:25:43 +0200

    Initial Commit

---
+++ BraintriTask/Controller/SearchVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BraintriTask
+//
+//  Created by Filip on 05/05/2019.
+//  Copyright © 2019 Filip. All rights reserved.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
